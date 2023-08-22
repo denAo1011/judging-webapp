9bdd7eb3e6571b0b155a1ec506d7423e60fda44d
John Mark Ovalo <johnmarkovalo@gmail.com>
2023-08-23 00:38:06 +0800

    feat: Footer

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,4 +2,8 @@
     <div class="w-full">
         <router-view> </router-view>
     </div>
+    <Footer></Footer>
 </template>
+<script setup>
+import Footer from "./components/Footer.vue";
+</script>
