e3de48082a0c337e0730a4289fefb0bc581f92e0
nthnyvllflrs <anthonvillaflor@gmail.com>
2023-08-31 13:48:18 +0800

    feat: add jurror count

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\CompanyEntry;
+use App\Models\CompanyJuror;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -21,8 +22,10 @@ class DashboardController extends Controller
             'approved' => CompanyEntry::whereStatus('APPROVED')->count(),
         ];
 
+        $jurorCount = CompanyJuror::count();
+
         return response()
-            ->json(['entries' => $numberOfEntries]);
+            ->json(['entries' => $numberOfEntries, 'jurors' => $jurorCount]);
     }
 
     /**
